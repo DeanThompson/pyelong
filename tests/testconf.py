f3f867641f1a5ccab95c65ff18f995f79de04bf2
DeanThompson <yanglianglee@gmail.com>
2015-06-18 19:17:38 +0800

    update

--- tests/testconf.py
+++ tests/testconf.py
@@ -2,10 +2,9 @@
 
 from pyelong import Client
 
-
 client = Client(
-    user='Agent1434522465',
-    app_key='13776ec2bfbd7d97389a528fed5df3da',
-    secret_key='c3c39c3ac3a29fef00aa50bd995a15c4',
-    host='api.test.lohoo.com/rest'
+    user='user',
+    app_key='app_key',
+    secret_key='secret_key',
+    host='api.test.lohoo.com/rest'  # 测试环境
 )
